2ba26a03e8fcc2743740af3784e1e7203535747d
Mikołaj Szczepkowski <mikolajszczepkowski@gmail.com>
2015-12-21 12:27:24 +0100

    menu ajax display

--- js/terms_app.js
+++ js/terms_app.js
@@ -5,4 +5,20 @@ $(function (){
 			$(this).addClass("active");
 		}
 	});
+	$("nav a").on("click", function(e){
+		e.preventDefault();
+		var url = this.href;
+
+		$("nav a.active").removeClass("active");
+		$(this).addClass("active");
+
+		$.ajax({
+			url: url,
+			dataType: "html",
+			type: "GET",
+			success: function(data){
+				$(".bottom-main").html($(data).find(".bottom-inner-wrapper")).hide().fadeIn(500);
+			}
+		});
+	});
 });
